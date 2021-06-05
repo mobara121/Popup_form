ace386f7ccfc7e62637dc456cd39a5eb17d43487
mobara121 <mizueo121@gmail.com>
2021-06-05 14:57:32 -0400

    modal changed

--- src/ModalComponent.jsx
+++ src/ModalComponent.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core';
 import Modal from '@material-ui/core/Modal';
+import TextField from '@material-ui/core/TextField';
   
 function getModalStyle() {
     const top = 50;
@@ -37,14 +38,15 @@ const useStyles = makeStyles ((theme)=>({
         padding: theme.spacing(2, 4, 3),
     },
     itemContainer: {
-        display:'flex', flexWrap: 'wrap', fontSize: '1rem', justifyContent:'center', marginBottom:'20px'
+        display:'flex', flexWrap: 'wrap', fontSize: '1rem', justifyContent:'center', marginBottom:'15px'
     },
     itemName: {
         width: '250px',fontSize: '0.8rem', textAlign:'left', margin:'3px auto', fontWeight:'bold'
     },
     item: {
-        width:'200px', height:'30px'
-    }
+        margin: theme.spacing(1),
+        top:5,
+      },
 }))
 
 const ModalComponent = () => {
@@ -65,20 +67,50 @@ const ModalComponent = () => {
           <h2 style={{textAlign:'center'}}>CREATE YOUR ACCOUNT</h2>
           <form style={{textAlign:'center'}}>
               <div className={classes.itemContainer}>
-                <p className={classes.itemName}>First Name</p><input className={classes.item}></input>
-                <p className={classes.itemName}>Last Name</p><input className={classes.item}></input>
-              </div>
-              <div className={classes.itemContainer}>
-                <p className={classes.itemName}>Email Address</p><input className={classes.item}></input>
-                <p className={classes.itemName}>Confirm Email Address</p><input className={classes.item}></input>
-              </div>
-              <div className={classes.itemContainer}>
-                <p className={classes.itemName}>Create a password</p><input className={classes.item}></input>
-                <p className={classes.itemName}>Confirm password</p><input className={classes.item}></input>
-              </div>
+                <TextField
+                        className={classes.item}
+                        label="First Name"
+                        required
+                        variant="outlined"
+                    />
+                    <TextField
+                        className={classes.item}
+                        label="Last Name"
+                        required
+                        variant="outlined"
+                    />
+                </div>
+                <div className={classes.itemContainer}>
+                    <TextField
+                        className={classes.item}
+                        label="Email"
+                        required
+                        variant="outlined"
+                    />
+                    <TextField
+                        className={classes.item}
+                        label="Confirm Email"
+                        required
+                        variant="outlined"
+                    />
+                </div>
+                <div className={classes.itemContainer}>
+                    <TextField
+                        className={classes.item}
+                        label="Password"
+                        required
+                        variant="outlined"
+
+                    />
+                    <TextField
+                        className={classes.item}
+                        label="Confirm Password"
+                        required
+                        variant="outlined"
+                    />
+                </div>
               <button className="btn" style={{margin:'20px auto', width: '80px', minWidth:'80px', height:'30px'}}>Submit!</button>
           </form>
-          {/* <ModalComponent /> */}
         </div>
       );
 
